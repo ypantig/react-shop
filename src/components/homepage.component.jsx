61572c53613ff3a65536e53d8068f8712e3abe6a
Ynah Pantig <me@ynahpantig.com>
2021-02-18 17:01:51 -0800

    Initial commit - create react app and start homepage

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage container'>
+    <div class='category-menu row'>
+      <div className='menu-item col-12 col-md-4 mb-4'>
+        <div className='inner border d-flex align-items-center justify-content-center'>
+          <div className='content px-3 py-3 border text-center'>
+            <h3>Hats</h3>
+            <span class='subtitle'>Shop Now</span>
+          </div>
+        </div>
+      </div>
+      <div className='menu-item col-12 col-md-4 mb-4'>
+        <div className='inner border d-flex align-items-center justify-content-center'>
+          <div className='content px-3 py-3 border text-center'>
+            <h3>Jackets</h3>
+            <span class='subtitle'>Shop Now</span>
+          </div>
+        </div>
+      </div>
+      <div className='menu-item col-12 col-md-4 mb-4'>
+        <div className='inner border d-flex align-items-center justify-content-center'>
+          <div className='content px-3 py-3 border text-center'>
+            <h3>Clothes</h3>
+            <span class='subtitle'>Shop Now</span>
+          </div>
+        </div>
+      </div>
+      <div className='menu-item col-12 col-md-6 mb-4'>
+        <div className='inner border d-flex align-items-center justify-content-center'>
+          <div className='content px-3 py-3 border text-center'>
+            <h3>Men</h3>
+            <span class='subtitle'>Shop Now</span>
+          </div>
+        </div>
+      </div>
+      <div className='menu-item col-12 col-md-6 mb-4'>
+        <div className='inner border d-flex align-items-center justify-content-center'>
+          <div className='content px-3 py-3 border text-center'>
+            <h3>Women</h3>
+            <span class='subtitle'>Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
